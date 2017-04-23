91e22842b0ce49c2ce8ff508846a81692d9f2abf
William Fung <19wf2@tcl217a-24.cs.williams.edu>
2017-04-22 20:59:31 -0400

    hello this is groot

--- LexiconTrie.java
+++ LexiconTrie.java
@@ -11,14 +11,33 @@ public class LexiconTrie implements Lexicon {
       //I am Groot
       this.groot = new LexiconNode(' ', false);
       this.wordList = new Vector<String>();
+
     }
 
 
     public boolean addWord(String word) {
-      if (containsWord(word)){
+      if (this.wordList.contains(word)){
         return false;
       } else {
         this.wordList.add(word);
+        LexiconNode currentParent = groot;
+        for (int i = 0; i < word.length(); i++){
+          char ch = word.charAt(i);
+          if (currentParent.getChild(ch) == null){
+            //i am groot
+            LexiconNode babygroot;
+            if (word.length() == 1){
+              babygroot = new LexiconNode(ch, true);
+            } else {
+              babygroot = new LexiconNode(ch, false);
+            }
+            //addChild already checks for duplicates
+            currentParent.addChild(babygroot);
+            currentParent = babygroot;
+          } else {
+            currentParent = currentParent.getChild(ch);
+          }
+        }
         return true;
       }
     }
@@ -36,7 +55,7 @@ public class LexiconTrie implements Lexicon {
     }
 
     public boolean removeWord(String word) {
-      if (containsWord(word)){
+      if (this.wordList.contains(word)){
         this.wordList.remove(word);
         return true;
       } else {
@@ -80,13 +99,22 @@ public class LexiconTrie implements Lexicon {
       return null;
     }
 
+
+    //We are Groot.
     public static void main(String args[]){
       LexiconTrie chrisPratt =  new LexiconTrie();
-      chrisPratt.addWordsFromFile("small.txt");
-      System.out.println(chrisPratt.wordList.toString());
-      chrisPratt.removeWord("top");
-      System.out.println(chrisPratt.wordList.toString());
+      //chrisPratt.addWordsFromFile("small.txt");
+      //System.out.println(chrisPratt.wordList.toString());
+      //chrisPratt.removeWord("top");
+      //System.out.println(chrisPratt.wordList.toString());
       System.out.println(chrisPratt.numWords());
       System.out.println(chrisPratt.containsPrefix("so"));
+      chrisPratt.addWord("potato");
+      chrisPratt.addWord("wow");
+      chrisPratt.addWord("wham");
+      System.out.println("Node to string: " + chrisPratt.groot.node.toString());
+      System.out.println("Node to string: " + chrisPratt.groot.getChild('w').getChild('o').letter);
+      System.out.println("Node size of w: " + chrisPratt.groot.getChild('w').node.size());
+      System.out.println("Node to string: " + chrisPratt.groot.getChild('w').getChild('h').letter);
     }
 }
